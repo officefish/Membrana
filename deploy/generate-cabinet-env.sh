930535c1b9779f9e249e908f54102dfc39f3bb9d
druid <indicogames@yandex.ru>
2026-09-22 17:59:54 +0300

    docs(cabinet): document office proxy env

--- deploy/generate-cabinet-env.sh
+++ deploy/generate-cabinet-env.sh
@@ -32,6 +32,7 @@ CLIENT_CORS_ORIGINS=http://localhost:5173,http://localhost:5174,http://localhost
 MEDIA_API_URL=http://media-api:3010
 MEDIA_PUBLIC_API_URL=https://media.membrana.space
 # Office registration pair: set OFFICE_URL and OFFICE_API_TOKEN manually from the office host secret.
+# Optional office egress proxy: HTTPS_PROXY / HTTP_PROXY are honored by the office registration bridge when set.
 ALLOW_REGISTRATION=false
 CABINET_RUN_SEED=true
 CABINET_BOOTSTRAP_LOGIN=admin
